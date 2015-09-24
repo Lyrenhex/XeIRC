7b9b81310a940b3c03139d358b471452c8e62f83
Damian Heaton <adonis@icronium.com>
2015-09-24 20:19:35 +0100

    Chat Improvements
    - XeIRC now logs all chat to files.
    - Added /names command, which lists all users on the current channel

--- client.py
+++ client.py
@@ -18,6 +18,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+# colours
+# [56] [2015.09.19 21:43:49] [#8bit] <Scratso^logs> XeIRC notes: \x031black \x032blue \x033green \x034red \x035brown \x036purple \x037orange \x038yellow \x039lightgreen \x0310teal/turquoise-y? \x030white
+
 import socket
 import os
 import sys
@@ -68,6 +71,14 @@ secondary = []
 
 ctypes.windll.kernel32.SetConsoleTitleW("XeIRC - "+botnick+" @ "+server+":"+str(port))
 
+if os.path.isdir("logs") == False:
+    os.mkdir("logs")
+if os.path.isdir("logs/"+botnick) == False:
+    os.mkdir("logs/"+botnick)
+if os.path.isdir("logs/"+botnick+"/"+server) == False:
+    os.mkdir("logs/"+botnick+"/"+server)
+logdir = "logs/"+botnick+"/"+server
+
 msgs = []
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -79,7 +90,7 @@ irc.send(bytes("NICK "+ botnick +"\n", "utf-8"))
 text = str(irc.recv(2040))
 unraw=text.split("\\r\\n")
 for line in unraw:
-    print(line)
+    #print(line)
     try:
         nick = line.split(":")[1].split("!")[0]
         text = line.split(":", 2)[2]
@@ -110,6 +121,8 @@ def send():
                 irc.send(bytes("JOIN "+ sendt.split(" ", 1)[1] +"\n", "utf-8"))
             elif "/quit" in sendt:
                 irc.send(bytes("QUIT :"+ sendt.split(" ", 1)[1] +"\n", "utf-8"))
+            elif "/names" in sendt:
+                irc.send(bytes("NAMES :"+ channel +"\n", "utf-8"))
             elif "/away" in sendt:
                 irc.send(bytes("AWAY :"+ sendt.split(" ", 1)[1] +"\n", "utf-8"))
                 irc.send(bytes("NICK :"+ botnick +"^away\n", "utf-8"))
@@ -163,6 +176,9 @@ while True:
                 msg = "["+time.strftime("%Y.%m.%d %H:%M:%S")+"] ["+chan+"] <"+nick+"> "+text
                 msgs.append(msg)
                 print("["+str(msgs.index(msg))+"] "+msgs[msgs.index(msg)])
+                file = open(logdir+"/"+chan+".log", "a")
+                file.write("["+str(msgs.index(msg))+"] "+msgs[msgs.index(msg)]+"\n")
+                file.close()
                 if botnick in text:
                     print("\a", end="")
         except Exception as e:
@@ -199,5 +215,7 @@ while True:
                     +" has changed their name: "+line.split(":", 2)[2]+"."
                 msgs.append(msg)
                 print("["+str(msgs.index(msg))+"] "+msgs[msgs.index(msg)])
+        elif line.find("353") != -1:
+            print("*** People currently on channel: "+text)
 
    #thread.start_new_thread(listen, ())
